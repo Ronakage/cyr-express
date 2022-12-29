280ba6559f91e32434a529840aed4623355ea3c6
Rony <ronymawad@gmail.com>
2022-12-29 16:51:16 -0500

    Fix scrolling on login page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -47,86 +47,88 @@ class _AuthentactionPageState extends State<AuthentactionPage> {
     return Scaffold(
       body: Center(
         child:
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Company name
-            Text("Cyr Express", 
-                style: GoogleFonts.poppins(
-                color: const Color.fromARGB(255, 59, 59, 61),
-                fontSize: 75,
-                fontWeight: FontWeight.w900,
-              ), 
-            ),
-            const SizedBox(height: 50),
-            //E-mail Text Field
-            const SizedBox(
-              width: 500,
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                maxLines: 1,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: CustomColors.primaryColor),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: CustomColors.primaryColor),
-                    borderRadius: BorderRadius.all(Radius.circular(100))
-                  ),
-                  hintText: 'E-mail',
-                )
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Company name
+              Text("Cyr Express", 
+                  style: GoogleFonts.poppins(
+                  color: const Color.fromARGB(255, 59, 59, 61),
+                  fontSize: 75,
+                  fontWeight: FontWeight.w900,
+                ), 
               ),
-            ),
-            const SizedBox(height: 10),
-            //Password Text Field
-            const SizedBox(
-              width: 500,
-              child: TextField(
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                maxLines: 1,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: CustomColors.primaryColor),
-                    borderRadius: BorderRadius.all(Radius.circular(100))
-                  ),
-                  hintText: 'Password',
-                )
-              ),
-            ),
-            const SizedBox(height: 50),
-            //Login Button
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: CustomColors.primaryColor,
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
+              const SizedBox(height: 50),
+              //E-mail Text Field
+              const SizedBox(
+                width: 500,
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: CustomColors.primaryColor),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: CustomColors.primaryColor),
+                      borderRadius: BorderRadius.all(Radius.circular(100))
+                    ),
+                    hintText: 'E-mail',
+                  )
                 ),
-                minimumSize: const Size(200, 70), 
               ),
-              onPressed: _login,
-              child: const Text("Login", style: TextStyle(fontSize: 20),),
-            ),
-            const SizedBox(height: 15),
-            const SizedBox(height: 50 ,width: 100 ,child: Divider(color: Colors.grey)),
-            const Text("You don't have an account yet? Register now!"),
-            const SizedBox(height: 50),
-            //Register Button
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: CustomColors.primaryColor,
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0)
+              const SizedBox(height: 10),
+              //Password Text Field
+              const SizedBox(
+                width: 500,
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: CustomColors.primaryColor),
+                      borderRadius: BorderRadius.all(Radius.circular(100))
+                    ),
+                    hintText: 'Password',
+                  )
                 ),
-                minimumSize: const Size(200, 50), 
               ),
-              onPressed: _register,
-              child: const Text("Register", style: TextStyle(fontSize: 20),),
-            ),
-          ],
+              const SizedBox(height: 50),
+              //Login Button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: CustomColors.primaryColor,
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  minimumSize: const Size(200, 70), 
+                ),
+                onPressed: _login,
+                child: const Text("Login", style: TextStyle(fontSize: 20),),
+              ),
+              const SizedBox(height: 15),
+              const SizedBox(height: 50 ,width: 100 ,child: Divider(color: Colors.grey)),
+              const Text("You don't have an account yet? Register now!"),
+              const SizedBox(height: 50),
+              //Register Button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: CustomColors.primaryColor,
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)
+                  ),
+                  minimumSize: const Size(200, 50), 
+                ),
+                onPressed: _register,
+                child: const Text("Register", style: TextStyle(fontSize: 20),),
+              ),
+            ],
+          ),
         )  
       ),
     );
